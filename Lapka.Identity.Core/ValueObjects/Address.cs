d5550d63b1f5f319d7ddab66fa32fe4c0c9b70c2
Quazowski <wardls33@gmail.com>
2021-08-03 12:57:39 +0200

    Address and location

---
+++ Lapka.Identity.Core/ValueObjects/Address.cs
@@ -0,0 +1,10 @@
+namespace Lapka.Identity.Core.ValueObjects
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+            
+    }
+}
---
+++ Lapka.Identity.Core/ValueObjects/Location.cs
@@ -0,0 +1,8 @@
+namespace Lapka.Identity.Core.ValueObjects
+{
+    public class Location
+    {
+        public string Latitude  { get; set; }
+        public string Longitude   { get; set; }
+    }
+}
