71a67dbea14379cc9ff185729b95bba3115fe91b
Quazowski <wardls33@gmail.com>
2021-08-13 15:15:28 +0200

    changed location validation

---
+++ Lapka.Identity.Core/Exceptions/Location/LongitudeIncorrectDataTypeException.cs
@@ -0,0 +1,15 @@
+﻿using Lapka.Identity.Core.Exceptions.Abstract;
+
+namespace Lapka.Identity.Core.Exceptions.Location
+{
+    public class LongitudeIncorrectDataTypeException : DomainException
+    {
+        public string Longitude { get; }
+        public LongitudeIncorrectDataTypeException(string longitude) : base($"Invalid data type of longitude: {longitude}")
+        {
+            Longitude = longitude;
+        }
+
+        public override string Code => "invalid_longitude_data_type";
+    }
+}
